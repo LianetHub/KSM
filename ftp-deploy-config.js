4502ab010ff9ce31627f7f53cab92f44ab0e4aa5
lianet <jack.tolmak@yandex.ru>
2024-07-08 10:55:24 +0300

    commit

--- ftp-deploy-config.js
+++ ftp-deploy-config.js
@@ -2,18 +2,18 @@ const FtpDeploy = require("ftp-deploy");
 const ftpDeploy = new FtpDeploy();
 
 const config = {
-  user: "your-ftp-username",
-  password: "your-ftp-password",
-  host: "your-ftp-host",
+  user: "ksm@inthehood",
+  password: "H1mdui2dPqksm",
+  host: "141.8.193.210",
   port: 21,
-  localRoot: __dirname + "/build",
+  localRoot: __dirname + "/KSM",
   remoteRoot: "/path/to/remote/directory",
   include: ["*", "**/*"], // This uploads all files
   deleteRemote: false, // Set this to true if you want to delete existing files
   forcePasv: true // Optional for some servers
 };
 
-ftpDeploy.deploy(config, function(err) {
+ftpDeploy.deploy(config, function (err) {
   if (err) console.log(err);
   else console.log("finished");
 });
